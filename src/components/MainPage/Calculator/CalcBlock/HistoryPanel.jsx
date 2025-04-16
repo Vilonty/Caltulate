890953b81823f9728a842d2b80b9ccfc3e872940
him1k0ta <149035975+him1k0ta@users.noreply.github.com>
2025-04-16 22:09:20 +0700

    lastv

--- src/components/MainPage/Calculator/CalcBlock/HistoryPanel.jsx
+++ src/components/MainPage/Calculator/CalcBlock/HistoryPanel.jsx
@@ -1,19 +1,21 @@
-import styles from '../../../../styles/MainPage/ThemeButton.module.css';
+import themeStyles from '../../../../styles/MainPage/ThemeButton.module.css';
+import historyStyles from '../../../../styles/MainPage/Calculator/HistoryPanel.module.css';
+
 export const HistoryPanel = ({ history, onClearHistory, onSelectHistory, theme }) => {
   return (
-    <div className={`${styles.HistoryPanel} ${styles[theme]}`}>
-      <div className={`${styles.HistoryHeader} ${styles[theme]}`}>
+    <div className={`${historyStyles.HistoryPanel} ${historyStyles[theme]}`}>
+      <div className={`${historyStyles.HistoryHeader} ${historyStyles[theme]}`}>
         <h3>История вычислений</h3>
         <button onClick={onClearHistory}>Очистить</button>
       </div>
-      <div className={styles.HistoryListContainer}>
-        <div className={`${styles.HistoryListWrapper} ${styles[theme]}`}>
-          <ul className={styles.HistoryList}>
+      <div className={historyStyles.HistoryListContainer}>
+        <div className={`${historyStyles.HistoryListWrapper} ${historyStyles[theme]}`}>
+          <ul className={historyStyles.HistoryList}>
             {history.map((item, index) => (
               <li key={index} onClick={() => onSelectHistory(item)}>
-                <div className={styles.HistoryCalculation}>{item.calculation} =</div>
-                <div className={styles.HistoryResult}>{item.result}</div>
-                <div className={styles.HistoryTime}>
+                <div className={historyStyles.HistoryCalculation}>{item.calculation} =</div>
+                <div className={historyStyles.HistoryResult}>{item.result}</div>
+                <div className={historyStyles.HistoryTime}>
                   {new Date(item.timestamp).toLocaleTimeString()}
                 </div>
               </li>
